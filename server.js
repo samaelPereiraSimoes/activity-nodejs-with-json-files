4050e9a1b078c3b0e1ee2fe013b3593c18c0af10
Samael Pereira Simoes <samael.simoes@gmail.com>
2018-03-25 16:39:56 -0300

    Ajustes calculo

--- server.js
+++ server.js
@@ -12,7 +12,8 @@ var server = http.createServer(function(request, response) {
 		objCatalog = JSON.parse(fs.readFileSync('public/catalog.json', 'utf8'));
 		objPurchases = JSON.parse(fs.readFileSync('public/purchases.json', 'utf8'));
 
-		var valueSalesCatalog = salesCatalog(objSales, objCatalog)
+		var valueSalesCatalog = salesCatalog(objSales, objCatalog);
+console.log(valueSalesCatalog); 	
 	}
 
 	fs.readFile('./public/' + page, function(err, data) {
@@ -33,17 +34,21 @@ function salesCatalog (objSales, objCatalog) {
 
 	salesCatalog = [];
 	objSales.map( function(sale) { //varrendo as vendas 
-		sale.catalog = objCatalog.filter(function(catalog){  // após varrer vou pegar o produto
+		sale.catalog = objCatalog.filter(function(catalog){ //após varrer vou pegar o produto
+			
 			return sale.product_id === catalog.id.toString();
 		});
 
-		sale.qtproduto = sale.catalog.length; // pegando quantidade de produto
-		sale.total = parseInt(sale.price) * sale.qtproduto; // calculando o total pela quantidade
-	
+		sale.qtproduto = sale.catalog.length; //pegando quantidade de produto
+		
+		if (sale.catalog[0] != "" && sale.catalog[0] != undefined) {
+
+			sale.total = parseFloat(sale.catalog[0].price) * sale.qtproduto
+		}	
 		salesCatalog.push(sale);
+		
 	});
 
-	console.log(salesCatalog);
 	return salesCatalog;
 }
 
@@ -56,4 +61,22 @@ server.listen(3000);
 			preço, 
 			calcular o total pela quantidade
 		e após separar por datas
+*/
+
+
+/*
+
+ logica da data
+Data de fechamento do cartão de credito é: dia 5 de cada mes
+Parcela cai dia 10
+se uma compra foi feito o pagamento dia 05 9 2017 primeira parcela cai dia 10 10 2017
+se tivesse sido dia 04 09 2017 cairia dia 10 09 2017.
+
+vou pegar o dia e o mes separado validar e nisso salvar td em um array separado
+
+var data = new Date('2017-12-14 21:39:39');
+var dia = data.getDate();
+var mes = data.getMonth() + 1;
+var ano = data.getFullYear();
+
 */
